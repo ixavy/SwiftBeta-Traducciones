643bb0cbbbfa8efc6a88f96210fe2cfaf1b1f662
iXavy <ict@ixavy.com>
2022-11-23 18:44:57 +0100

    Initial Commit

---
+++ Traducciones/TraduccionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraduccionesApp.swift
+//  Traducciones
+//
+//  Created by iXavy on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TraduccionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
