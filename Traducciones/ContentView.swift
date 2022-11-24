44e2908935ed9bdefbb8b739d2528d6047a351b7
iXavy <ict@ixavy.com>
2022-11-24 10:00:23 +0100

    Finished example

--- Traducciones/ContentView.swift
+++ Traducciones/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textFieldName: String = ""
+    @State var textFieldPhone: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            Form {
+                Section {
+                    TextField("form-name-key", text: $textFieldName)
+                    TextField("form-phone-user \(textFieldName)", text: $textFieldPhone)
+                        .keyboardType(.namePhonePad)
+                    HStack {
+                        Spacer()
+                        Button("form-button-key") {
+                            print("Save information...")
+                        }
+                            .buttonStyle(.bordered)
+                            .tint(.blue)
+                    }
+                } header: {
+                    Text("form-header-key")
+                } footer: {
+                    Text("form-footer-key")
+                }
+            }
+                .tabItem {
+                    Label("tabview-home-key", systemImage: "house.fill")
+                }
+            Text("Profile")
+                .tabItem {
+                    Label("tabview-profile-key", systemImage: "person.crop.circle.fill")
+                }
         }
-        .padding()
     }
 }
 
